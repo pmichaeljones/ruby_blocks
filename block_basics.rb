72493b94099bc76bc18680028161119857e8d83e
Patrick Jones <pmichaeljones@gmail.com>
2015-01-28 13:14:14 -0700

    added intro lesson code

--- block_basics.rb
+++ block_basics.rb
@@ -1,1 +1,28 @@
+class Order
+
+  attr_reader :customer, :cost
+
+  def initialize(customer, cost)
+    @customer = customer
+    @cost = cost
+  end
+
+  def to_s
+    "This order for #{customer} cost $#{cost}"
+  end
+
+end
+
 puts "Let's start mastering Ruby blocks!"
+
+orders = []
+
+3.times do
+  orders << Order.new("Patrick", 23)
+end
+
+puts orders
+
+
+
+
