2d6d971d5641aaf666990342275b001d78dae074
Patrick Jones <pmichaeljones@gmail.com>
2015-02-18 10:17:34 -0500

    included scores array

--- card.rb
+++ card.rb
@@ -13,3 +13,9 @@ cards.shuffle.each { |card| puts "#{card.upcase} - #{card.length}" }
 puts "-----"
 
 cards.reverse_each { |card| puts "#{card.upcase} - #{card.length}" }
+
+puts "-----"
+
+scores = {"Larry" => 10, "Moe" => 8, "Curly" => 12}
+
+scores.each{ |k,v| puts "#{k} got a score of #{v}!"}
